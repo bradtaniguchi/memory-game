c049d580cc41fd3f6f03805a4668cbddfdc67bb8
GummyGod <gummy.pwnn@yahoo.com>
2018-03-23 15:49:33 +0200

    added shuffle function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 //declare variables
 let card = document.querySelectorAll(".card");
+let cards = [];
+// function to convert a nodelist to array from https://stackoverflow.com/questions/3199588/fastest-way-to-convert-javascript-nodelist-to-array
+for(var i = card.length; i--; cards.unshift(card[i]));
+console.log(cards);
 const deck = document.querySelector(".deck");
 
 
@@ -10,6 +14,10 @@ for(var i = 0; i< card.length;i++){
     this.classList.toggle("show");
   });
 };
+
+//Call the startGame function to start the game and shuffle cards
+window.onload = startGame();
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -27,9 +35,11 @@ function shuffle(array) {
 }
 
 //shuffle cards
-function start() {
-  // This shuffle function shuffles the cards
-  let allCards = shuffle(objects);
+function startGame(){
+   var shuffledCards = shuffle(cards);
+   for (var i= 0; i < shuffledCards.length; i++){
+      [].forEach.call(shuffledCards, function(item){
+         deck.appendChild(item);
+      });
+   }
 }
-
-start();
