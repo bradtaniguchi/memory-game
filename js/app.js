95bfd454e6ef35c4e1f057e8f48f8eec237ff129
GummyGod <gummy.pwnn@yahoo.com>
2018-03-23 02:48:53 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let cards = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'];
+let card = document.querySelectorAll(".card");
+const deck = document.querySelector(".deck");
+
+
+
+for(var i = 0; i< card.length;i++){
+  card[i].addEventListener("click", function(){
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+  });
+};
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
